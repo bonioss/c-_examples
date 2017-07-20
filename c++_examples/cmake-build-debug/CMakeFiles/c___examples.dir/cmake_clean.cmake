ca604028791365f9cf37c6700648ea6399c0e4d2
Patryk Bochnak <slash98.pb@gmail.com>
2017-07-21 01:45:05 +0200

    create new project

---
+++ c++_examples/cmake-build-debug/CMakeFiles/c___examples.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/c___examples.dir/main.cpp.obj"
+  "c___examples.pdb"
+  "c___examples.exe"
+  "c___examples.exe.manifest"
+  "libc___examples.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/c___examples.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
