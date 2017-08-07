4d94a7e2a321bdf4c5c73a89e3df1c342f718335
Patryk Bochnak <slash98.pb@gmail.com>
2017-08-07 21:02:39 +0200

    double

---
+++ double/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>
+
+int main ()
+{
+using namespace std;
+
+double area;
+    cout << "Podaj powierzchnie swojego mieszkania w metrach kwadratowych: ";
+    cin >> area;
+    double side;
+    side = sqrt (area);
+    cout << "Odpowiada to kwadratowi o boku " << side
+                                              << " metrow." << endl;
+    cout << "Niesamowite!" << endl;
+    return 0;
+}
